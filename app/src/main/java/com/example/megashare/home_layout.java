4f82c85f61ab4834b70b2f5244b7584a9eef0457
Chetan Jadhav <143217097+chetanjadhav0126@users.noreply.github.com>
2024-09-04 21:40:46 +0530

    Adding Group share, files and account ta. Also adding the login and signup pages(Under Wrokking)

--- app/src/main/java/com/example/megashare/home_layout.java
+++ app/src/main/java/com/example/megashare/home_layout.java
@@ -1,6 +1,9 @@
 package com.example.megashare;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,42 @@ public class home_layout extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ImageButton individualbtnimg = findViewById(R.id.individualbtnimg);
+        individualbtnimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent individualbtnimg = new Intent(home_layout.this, home_layout.class);
+                startActivity(individualbtnimg);
+            }
+        });
+
+        ImageButton groupbtnimg = findViewById(R.id.groupbtnimg);
+        groupbtnimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent groupbtnimg = new Intent(home_layout.this, group_share.class);
+                startActivity(groupbtnimg);
+            }
+        });
+
+        ImageButton accountbtnimg = findViewById(R.id.accountbtnimg);
+        accountbtnimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent accountbtnimg = new Intent(home_layout.this, account.class);
+                startActivity(accountbtnimg);
+            }
+        });
+
+        ImageButton filesbtnimg = findViewById(R.id.filesbtnimg);
+        filesbtnimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent filesbtnimg = new Intent(home_layout.this, files.class);
+                startActivity(filesbtnimg);
+            }
+        });
+
     }
 }
